c1b9d6c02a3ed469942bd3532d9f1a30f3a6aeb9
DuleepAlwis <duleepalwis@gmail.com>
2026-03-05 13:20:27 +0530

    My-profile web developped

---
+++ src/components/NavigationPanel.jsx
@@ -0,0 +1,45 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+
+
+export const NavigationPanel = ()=>{
+return (<>
+<Navbar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Profile</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" variant="blue">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#Tech stack">Technologies</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+                        <Nav.Link href="projects">Projects</Nav.Link>
+
+            
+
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      
+    </Navbar>
+
+    </>
+  )
+}
